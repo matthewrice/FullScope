599ceab84b416339267d4b933da40639c08d20a3
Matthew Rice <mtr.rice@gmail.com>
2016-08-01 06:31:52 -0400

    Updated Dashboard.  Still needs some tweaks.

--- app/scripts/components/app-header.jsx
+++ app/scripts/components/app-header.jsx
@@ -18,21 +18,17 @@ var AppHeader = React.createClass({
       <div>
         <nav className="row app-header">
           <div className="nav nav-bar">
-            <div className="col-xs-2 patientfamily-link">
-              <a href="#patientfamily">
-                <img className="family-icon" src="images/patient-family-link-icon.svg" />
-              </a>
-            </div>
-            <div className="col-xs-9 fullscope">
+            <div className="col-xs-offset-1 col-xs-10 fullscope">
               <a href="#dashboard">FullScope</a>
             </div>
             <div className="col-xs-1 account-links">
               <i className="fa fa-bars dropdown-toggle fontawesome-navbars" aria-hidden="true" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></i>
               <ul className="dropdown-menu">
-                <li><a href="#signup">Sign Up</a></li>
-                <li><a href="#login">Log In</a></li>
-                <li><a href="#supporter">Supporters</a></li>
-                <li><a href="#patientfamily">Patient Families</a></li>
+                <li className="menu"><a href="#signup">Sign Up</a></li>
+                <li className="menu"><a href="#login">Log In</a></li>
+                <li className="menu"><a href="#login">Log Out</a></li>
+                <li className="menu"><a href="#supporter">Supporters</a></li>
+                <li className="menu"><a href="#patientfamily">Patient Families</a></li>
               </ul>
             </div>
           </div>
--- app/scripts/components/dashboard.jsx
+++ app/scripts/components/dashboard.jsx
@@ -1,21 +1,81 @@
 var React = require('react');
 
+var PatientFamilyCollection = require('../models/patient-family-model').PatientFamilyCollection;
 var AppHeader = require('./app-header.jsx');
 
 
 // top level dashboard.jsx component
 var Dashboard = React.createClass({
+  getInitialState: function(){
+    return {
+      familyThumbnail: []
+    }
+  },
+  componentWillMount: function(){
+    var self = this;
+    var familyThumbnail = new PatientFamilyCollection();
 
+    familyThumbnail.fetch().done(function(){
+      self.setState({familyThumbnail: familyThumbnail});
+    });
+  },
+  handleClick: function(profile){
+    var router = this.props.router;
+    router.navigate('patientfamily/' + profile.get('objectId'), {trigger: true});
+  },
   render: function(){
+    var familyThumbnail = this.state.familyThumbnail;
+    var self=this;
+
+    var patientFamily = familyThumbnail.map(function(profile){
+      return (
+        <li onClick={function(){self.handleClick(profile)}} key={profile.get('objectId')} className="col-md-3 mini-profile-cards">
+          <img src="images/chloe.jpg" />
+          <div className="miniFamilyName">{profile.get('familyName')}</div>
+        </li>
+      );
+    });
 
     return(
       <div>
         <AppHeader />
 
-        <div className="row">
-          <div className="identity">Who We Are</div>
-          <div className="methods">Ways To Support</div>
-          <div className="families">Families We Support</div>
+        <div className="row hello">
+          <div className="">
+            <div className="purpose col-md-12">
+              <h3 className="purpose-title">Why We Exist</h3>
+              <p className="why-we-exist col-md-offset-2 col-md-8">Cancer is a burden that no family should have to bear on their own.
+                Full Scope exists to meet the needs a patient family may have, whether it be emotional,
+                financial, spiritual, or basic everyday needs. We are a community of people that understands
+                the impact cancer has on the entire family. Our goal is to provide a safe place
+                where patient families can share their story and have their <span className="emphasis">full scope </span>
+                of needs cared for.</p>
+            </div>
+            <div className="ways-to-get-involved col-md-12">
+              <div className="row">
+                <h3 className="ideas-title">Ways To Support A Family Facing Cancer.</h3>
+              </div>
+              <div className="row">
+                <div className="col-md-offset-5 col-md-2">
+                  <div className="ideas">- Invest your time</div>
+                  <div className="ideas">- Donate your services</div>
+                  <div className="ideas">- Offer spiritual support</div>
+                  <div className="ideas">- Meet a need</div>
+                  <div className="ideas financial-burden">- Relieve a financial burden</div>
+                </div>
+              </div>
+            </div>
+            <div className="families col-md-12">
+              <div className="row">
+                <h3 className="families-title">Families We Support</h3>
+              </div>
+              <div className="row">
+                <div className="col-md-offset-2 col-md-8 mini-profile-card-list-container">
+                  <ul className="mini-profile-card-list">{patientFamily}</ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
